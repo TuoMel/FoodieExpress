8d0cc61fc5c1b518612e15d0c8956d0fad44161d
Tuomas Mellin <tuomas@jmellin.net>
2024-01-04 18:38:18 +0200

    Create React Router to handle routing in the application. Create a user signup form with reusable buttons like "FoodieButton" and "FoodieInput". These can be used throughout the application. Develop the frontend in general, and remove bloatware.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './views/Home';
+import Restaurants from './views/Restaurants';
+import Signup from './views/Signup';
 
 function App() {
   return (
-      <div className="App">
-        <h1> Welcome to Foodie Express! </h1>
-      </div>
+      <>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants" element={<Restaurants />} />
+            <Route path="/register" element={<Signup />} />
+        </Routes>
+      </>
   );
 }
 
