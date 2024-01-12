e64cc4b7405e396a07509d116fc30af8c6cfa64f
Tuomas Mellin <tuomas@jmellin.net>
2024-01-12 18:00:07 +0200

    Implemented a JWT authentication to the project. For demonstration and test purposes, the access token is only valid for 15 seconds. Also, just the "getProfile" endpoint is protected. Testing and further development will continue.

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
+const { Authentication } = require("../../middleware/JWTAuthentication");
 
 router.delete('/:id', userController.deleteUser);
 
 router.get('/:id/orders', userController.getOrdersForUser);
-router.get('/:id/profile', userController.getProfile);
+router.get('/:id/profile', Authentication, userController.getProfile);
 router.put('/:id/profile', userController.updateProfile);
 
 
